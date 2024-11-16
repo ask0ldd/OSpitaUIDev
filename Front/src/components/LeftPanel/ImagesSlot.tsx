4a110aa810692278a1a4d10cadea0e8ec99b64a9
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-11-16 16:24:18 +0100

    *edit* front / closed image slot added

--- Front/src/components/LeftPanel/ImagesSlot.tsx
+++ Front/src/components/LeftPanel/ImagesSlot.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react"
 import { ImageRepository, Image } from "../../repositories/ImageRepository";
 
-function ImagesSlot({active} : IProps){
+function ImagesSlot({active, setActiveSlot} : IProps){
 
   const [images, setImages] = useState<Image[]>([])
 
@@ -29,8 +29,12 @@ function ImagesSlot({active} : IProps){
   }
 
   if(active == false) return(
-    <article style={{marginTop:'0.75rem'}}>
-      <h3>IMAGES<span className='nPages' style={{color:"#232323", fontWeight:'500'}}>open</span></h3>
+    <article style={{marginTop:'0.75rem', cursor:'pointer'}} onClick={() => setActiveSlot("images")}>
+      <h3>IMAGES
+        <svg style={{marginLeft:'auto', transform:'translateY(1px)'}} width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.0603 10.9405C10.4746 11.5258 9.52543 11.5258 8.93973 10.9405L1.30462 3.31105C0.359131 2.36626 1.02826 0.75 2.36489 0.75L17.6351 0.750001C18.9717 0.750002 19.6409 2.36627 18.6954 3.31105L11.0603 10.9405Z" fill="#353535"/>
+        </svg>
+      </h3>
     </article>
   )
 
@@ -48,4 +52,5 @@ export default ImagesSlot
 
 interface IProps{
   active : boolean
+  setActiveSlot : React.Dispatch<React.SetStateAction<"documents" | "images">>
 }
