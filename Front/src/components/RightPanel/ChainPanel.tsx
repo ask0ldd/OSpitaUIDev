59c50e724ef63a81315363700ce30f5102be0193
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-11-17 20:44:02 +0100

    *edit* front / chain panel : integration of a new version of the custom select

--- Front/src/components/RightPanel/ChainPanel.tsx
+++ Front/src/components/RightPanel/ChainPanel.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { AIAgent } from "../../models/AIAgent";
 import Select, { IOption } from "../CustomSelect/Select";
 import RightMenu from "./RightMenu";
 
-function ChainPanel({handleMenuItemClick, AIAgentsList,  currentChain, setCurrentChain, isStreaming} : IProps){
+function ChainPanel({handleMenuItemClick, AIAgentsList,  currentChain, setCurrentChain, isStreaming, memoizedSetModalStatus} : IProps){
     function handleSwitchChainAgent(option : IOption, id : string){
         setCurrentChain(chain => chain.map(link => link.selectId == id ? {selectId : link.selectId , agentName : option.value} : link))
     }
@@ -12,10 +13,14 @@ function ChainPanel({handleMenuItemClick, AIAgentsList,  currentChain, setCurren
         setCurrentChain(chain => [...chain.slice(0, index), ...chain.slice(index + 1)].map((link, index) => ({...link, selectId : "chainAgent" + index})))
     }
 
+    function handleEditChainAgent(){
+        memoizedSetModalStatus({visibility : true, contentId : "formSelectChainAgent"})
+    }
+
     return (<aside className="rightDrawer">
         <RightMenu handleMenuItemClick={handleMenuItemClick}/>
         <article className='chainContainer'>
-            <h3>CURRENT CHAIN</h3>
+            <h3 onClick={handleEditChainAgent}>CURRENT CHAIN</h3>
             <p>NB : Each Agent will process its predecessor's response using its own System Prompt. Finally, the last response will be displayed.</p>
             <div style={{width:'100%', height:'1px', borderBottom:'1px dashed #35353599', marginBottom:'1.25rem'}}></div>
             <div className="yourQuery">Your Query</div>
@@ -24,6 +29,7 @@ function ChainPanel({handleMenuItemClick, AIAgentsList,  currentChain, setCurren
             </svg>
             {currentChain.map((link, index) => (<div key={"agentArrowGroup"+index} style={{display:'flex', flexDirection:'column', width:'100%', alignItems:'center'}}>
                 <div className="chainSelectDeleteContainer">
+                    {/* overrideOnClickEvent={handleEditChainAgent} */}
                     <Select 
                         width="100%"
                         options={AIAgentsList.filter(agent => agent.getType() != "system" || agent.getName().includes("COTTableGenerator"))
@@ -47,7 +53,7 @@ function ChainPanel({handleMenuItemClick, AIAgentsList,  currentChain, setCurren
                     <path className={isStreaming ? "fadeAnim" : ""} fillRule="evenodd" clipRule="evenodd" d="M19.6023 20.4902L10.9602 29.5816C10.4299 30.1395 9.57009 30.1395 9.03977 29.5816L0.397739 20.4902C-0.13258 19.9323 -0.13258 19.0278 0.397739 18.4699C0.928058 17.912 1.78787 17.912 2.31819 18.4699L8.64203 25.1226L8.64204 -4.96474e-07L11.358 -3.77757e-07L11.358 25.1226L17.6818 18.4699C18.2121 17.912 19.0719 17.912 19.6023 18.4699C20.1326 19.0278 20.1326 19.9323 19.6023 20.4902Z" fill="#5CC9B9"/>
                 </svg>
             </div>))}
-            <button className='purpleShadow' onClick={() => setCurrentChain(chain => [...chain, {selectId : "chainAgent" + chain.length, agentName : "baseAssistant"}])}>+ Add Agent</button>
+            <button className='purpleShadow' onClick={() => setCurrentChain(chain => [...chain, {selectId : "chainAgent" + chain.length, agentName : "COTTableGenerator"}])}>+ Add Agent</button>
         </article>
     </aside>)
 }
@@ -60,4 +66,5 @@ interface IProps{
     currentChain : {selectId : string, agentName : string}[]
     setCurrentChain : React.Dispatch<React.SetStateAction<{selectId : string, agentName : string}[]>>
     isStreaming : boolean
+    memoizedSetModalStatus : ({visibility, contentId} : {visibility : boolean, contentId? : string}) => void
 }
