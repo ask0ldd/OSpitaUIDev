67d8f5cd52629cd2e92591e15b3a7ef7099a785f
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-11-18 16:06:54 +0100

    *edit* front / documents / images navigation

--- Front/src/components/LeftPanel/DocumentsSlot.tsx
+++ Front/src/components/LeftPanel/DocumentsSlot.tsx
@@ -111,9 +111,9 @@ export default function DocumentsSlot({isWebSearchActivated, setWebSearchActivat
     }
 
     if(active == false) return(
-        <article style={{marginTop:'0.75rem', cursor:'pointer'}} onClick={() => setActiveSlot("documents")}>
+        <article className="closedDocumentsSlot" style={{marginTop:'0.75rem', cursor:'pointer'}} onClick={() => setActiveSlot("documents")}>
         <h3>DOCUMENTS
-          <svg style={{marginLeft:'auto', transform:'translateY(1px)'}} width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg style={{marginLeft:'auto', transform:'translateY(1px)'}} width="16" height="10" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11.0603 10.9405C10.4746 11.5258 9.52543 11.5258 8.93973 10.9405L1.30462 3.31105C0.359131 2.36626 1.02826 0.75 2.36489 0.75L17.6351 0.750001C18.9717 0.750002 19.6409 2.36627 18.6954 3.31105L11.0603 10.9405Z" fill="#353535"/>
           </svg>
         </h3>
