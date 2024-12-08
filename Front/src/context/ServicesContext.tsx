5a05b61413677d49d2614509c4d15e0be6af132d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-12-08 02:39:09 +0100

    *edit* front / new services added to the servicesContext

--- Front/src/context/ServicesContext.tsx
+++ Front/src/context/ServicesContext.tsx
@@ -3,26 +3,30 @@ import PromptService from '../services/API/PromptService';
 import AgentService from '../services/API/AgentService';
 import { WebSearchService } from '../services/WebSearchService';
 import { TTSService } from '../services/TTSService';
+import ImageService from '../services/API/ImageService';
 
 export interface ServicesContextType {
   agentService: AgentService
   promptService: PromptService
   webSearchService: WebSearchService
   ttsService : TTSService
+  imageService : ImageService
 }
 
 const defaultContextValue: ServicesContextType = {
   agentService: new AgentService(),
   promptService: new PromptService(),
   webSearchService: new WebSearchService(),
-  ttsService : new TTSService()
+  ttsService : new TTSService(),
+  imageService : new ImageService(),
 };
 
 export const ServicesContext = createContext<ServicesContextType>({
   agentService: new AgentService(),
   promptService: new PromptService(),
   webSearchService: new WebSearchService(),
-  ttsService : new TTSService()
+  ttsService : new TTSService(),
+  imageService : new ImageService(),
 });
 
 interface ServicesProviderProps {
