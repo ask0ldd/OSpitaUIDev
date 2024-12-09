a0a3ce6b0a8cb16085a05bc200bf45d63681a218
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-12-09 02:59:02 +0100

    *edit* back / services : better error handling

--- Back/controllers/conversation.controller.js
+++ Back/controllers/conversation.controller.js
@@ -11,6 +11,7 @@ const saveConversation = (db) => async (req, res) => {
             lastAgentUsed : req.body.lastAgentUsed,
             lastModelUsed : req.body.lastModelUsed,
         })
+
         db.saveDatabase((err) => {
             if (err) {
                 console.error("Error saving database:", err)
@@ -24,11 +25,7 @@ const saveConversation = (db) => async (req, res) => {
 
     } catch (error) {
         console.error(error)
-        if (error.status) {
-        res.status(error.status).send(error.message)
-        } else {
         res.status(500).send('An error occurred while attempting to save the conversation')
-        }
     }
 }
 
@@ -72,11 +69,7 @@ const updateConversationById = (db) => async (req, res) => {
         }
       } catch (error) {
         console.error(error)
-        if (error.status) {
-          res.status(error.status).send(error.message)
-        } else {
-          res.status(500).send('An error occurred while attempting to update the agent')
-        }
+        res.status(500).send('An error occurred while attempting to update the agent')
       }
 }
   
@@ -84,9 +77,14 @@ const getConversationById = (db) => async (req, res) => {
     const conversationId = req.params.id
     if (!conversationId) return res.status(400).json({ error: 'conversation ID is required' })
     try {
-        const conversation = await db.getCollection('conversations').get(conversationId)
+        const conversationsCollection = await db.getCollection('conversations')
+        if (!conversationsCollection) {
+            return res.status(500).json({ error: 'conversations collection does not exist in the database.' })
+        }
+
+        const conversation = await conversationsCollection.get(conversationId)
         if (!conversation) return res.status(404).json({ error: 'The requested conversation was not found' })
-        console.log(JSON.stringify(conversation))
+        // console.log(JSON.stringify(conversation))
         return res.status(200).json({...conversation})
 
     } catch (error) {
@@ -97,45 +95,53 @@ const getConversationById = (db) => async (req, res) => {
 
 const getAllConversations = (db) => async (req, res) => {
     try {
-        console.log("Fetching conversation.")
-        const conversation = db.getCollection("conversations").find().reverse()
+        console.log("Fetching conversations.")
+        const conversationsCollection = await db.getCollection('conversations')
+        if (!conversationsCollection) {
+            return res.status(500).json({ error: 'conversations collection does not exist in the database.' })
+        }
 
-        return res.setHeader("Access-Control-Allow-Origin", "*").status(200).json(conversation)
+        const conversations = await conversationsCollection.find().reverse()
+
+        return res.setHeader("Access-Control-Allow-Origin", "*").status(200).json(conversations)
         
     } catch (error) {
         console.error("Error retrieving conversation:", error)
-        res.status(500).json({ message: 'An error occurred while retrieving conversation.' })
+        res.status(500).json({ message: 'An error occurred while retrieving conversations.' })
     }
 }
 
 const deleteConversationById = (db) => async (req, res) => {
-  const conversationId = req.params.id
-  if (!conversationId) {
-      return res.status(400).json({ error: 'conversation ID is required' })
-  }
+    const conversationId = req.params.id
+    if (!conversationId) {
+        return res.status(400).json({ error: 'conversation ID is required' })
+    }
 
-  try {
-      const collection = db.getCollection('conversations')
-      const conversation = await collection.get(conversationId)
+    try {
+        const conversationsCollection = await db.getCollection('conversations')
+        if (!conversationsCollection) {
+            return res.status(500).json({ error: 'conversations collection does not exist in the database.' })
+        }
+        const conversation = await conversationsCollection.get(conversationId)
 
-      if (!conversation) {
-          return res.status(404).json({ error: 'The requested conversation was not found' })
-      }
+        if (!conversation) {
+            return res.status(404).json({ error: 'The requested conversation was not found' })
+        }
 
-      await collection.remove(conversation)
+        await conversationsCollection.remove(conversation)
 
-      db.saveDatabase((err) => {
-          if (err) {
-              console.error("Error saving database:", err)
-              return res.status(500).json({ error: 'Cannot save the database' })
-          }
-          console.log("Database saved successfully")
-          return res.status(204).send()
-      })
-  } catch (error) {
-      console.error('Error deleting conversation:', error)
-      return res.status(500).json({ error: 'Internal server error' })
-  }
+        db.saveDatabase((err) => {
+            if (err) {
+                console.error("Error saving database:", err)
+                return res.status(500).json({ error: 'Cannot save the database' })
+            }
+            console.log("Database saved successfully")
+            return res.status(204).send()
+        })
+    } catch (error) {
+        console.error('Error deleting conversation:', error)
+        return res.status(500).json({ error: 'Internal server error' })
+    }
 }
 
 /*function createError(status, message) {
