a9278db53a1df74ac98efce06deeef0e132a9732
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-02 19:09:28 +0100

    *add* front / generated image service

---
+++ Front/src/services/API/GeneratedImagesService.ts
@@ -0,0 +1,42 @@
+import { IImage } from "../../interfaces/IImage"
+
+class GeneratedImagesService{
+    async getAll() : Promise<IImage[] | undefined>{
+        try {
+            const response = await fetch("/backend/generatedimages", {
+                method: "GET",
+                headers: { "Content-Type": "application/json", }
+            })
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+
+            return await response.json()
+            
+        } catch (error) {
+            console.error("Error fetching images list : ", error)
+            return undefined
+        }
+    }
+
+    async upload(formData : FormData): Promise<IImage | undefined>{
+        try{
+            formData.append("generated", "true")
+            const response = await fetch('backend/upload', {
+                method: 'POST',
+                body: formData
+            })
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+
+            return await response.json()
+        }catch(e){
+            console.error(e)
+        }
+    }
+}
+
+export default GeneratedImagesService
