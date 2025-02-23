e340394534c6f3b7ce3ae08a27a311846f646ef6
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-02-23 08:41:28 +0100

    *edit* front / drilling image gen into the right menu

--- Front/src/features/LeftPanel/ImageGenLeftPanel.tsx
+++ Front/src/features/LeftPanel/ImageGenLeftPanel.tsx
@@ -5,15 +5,24 @@ import ollama from '../../assets/Ollama3.png'
 import ComfyWorkflowsSlot from './ComfyWorkflowsSlot'
 import ComfyPromptsSlot from './ComfyPromptsSlot'
 
-function ImageGenLeftPanel() {
+function ImageGenLeftPanel({memoizedSetModalStatus, forceLeftPanelRefresh, selectedPromptNameRef} : IProps) {
 
     return(
-        <aside className="leftDrawer">
+        <aside key={"aside"+forceLeftPanelRefresh} className="leftDrawer">
             <figure style={{cursor:'pointer'}} onClick={() => location.reload()}><span>OSSPITA FOR</span> <img src={ollama}/></figure>
             <ComfyWorkflowsSlot/>
-            <ComfyPromptsSlot/>
+            <ComfyPromptsSlot memoizedSetModalStatus={memoizedSetModalStatus} selectedPromptNameRef={selectedPromptNameRef}/>
         </aside>
     )
 }
 
 export default ImageGenLeftPanel
+
+interface IProps{
+    memoizedSetModalStatus: ({ visibility, contentId }: {
+        visibility: boolean;
+        contentId?: string;
+    }) => void
+    forceLeftPanelRefresh: number
+    selectedPromptNameRef: React.MutableRefObject<string>
+}
